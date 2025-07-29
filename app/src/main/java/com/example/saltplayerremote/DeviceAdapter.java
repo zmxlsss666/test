ba71e0cc68267d7fa958ef877f6530ac16def57e
zmxlsss666 <154047141+zmxlsss666@users.noreply.github.com>
2025-07-30 07:38:19 +0800

    Update DeviceAdapter.java

--- app/src/main/java/com/example/saltplayerremote/DeviceAdapter.java
+++ app/src/main/java/com/example/saltplayerremote/DeviceAdapter.java
@@ -20,27 +20,34 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.DeviceView
     }
 
     public DeviceAdapter(List<NetworkDevice> deviceList, OnDeviceClickListener listener) {
-        this.deviceList = deviceList;
+        this.deviceList = deviceList != null ? deviceList : new ArrayList<>();
         this.listener = listener;
     }
 
     @NonNull
     @Override
     public DeviceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_1, parent, false);
         return new DeviceViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DeviceViewHolder holder, int position) {
-        NetworkDevice device = deviceList.get(position);
-        holder.deviceName.setText(device.getIp());
-        holder.itemView.setOnClickListener(v -> listener.onDeviceClick(device));
+        if (deviceList != null && position < deviceList.size()) {
+            NetworkDevice device = deviceList.get(position);
+            holder.deviceName.setText(device.getIp());
+            holder.itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    listener.onDeviceClick(device);
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
-        return deviceList.size();
+        return deviceList != null ? deviceList.size() : 0;
     }
 
     static class DeviceViewHolder extends RecyclerView.ViewHolder {
@@ -49,7 +56,9 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.DeviceView
         public DeviceViewHolder(@NonNull View itemView) {
             super(itemView);
             deviceName = itemView.findViewById(android.R.id.text1);
-            deviceName.setTextColor(itemView.getResources().getColor(android.R.color.white));
+            if (deviceName != null) {
+                deviceName.setTextColor(itemView.getResources().getColor(android.R.color.white));
+            }
         }
     }
 }
